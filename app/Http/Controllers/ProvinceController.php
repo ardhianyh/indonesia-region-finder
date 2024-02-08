67b8d8212a762e9cb748b1aa922870899107a31a
Andra <ardhian.hanum@gmail.com>
2024-02-08 21:52:09 +0700

    feat: swap implementation data-source cookie

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Province;
+use App\Utilities\RajaOngkir;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProvinceController extends Controller
 {
@@ -11,7 +14,13 @@ class ProvinceController extends Controller
    {
       try {
          $id = $request->query('id');
-         $province = $id ? Province::findOrFail($id) : Province::all();
+         $source = $request->cookie('data-source');
+
+         if ($source == "raja-ongkir") {
+            $province = $this->fromRajaOngkir($id);
+         } else {
+            $province = $this->fromLocal($id);
+         }
 
          return $this->responseJson(
             'success',
@@ -26,6 +35,27 @@ class ProvinceController extends Controller
             $th->getMessage(),
             null
          );
+      }
+   }
+
+   public function fromLocal($id)
+   {
+      try {
+         $province = $id ? Province::findOrFail($id) : Province::all();
+         return $province;
+      } catch (\Throwable $th) {
+         throw new Exception($th->getMessage(), 500);
+      }
+   }
+
+   public function fromRajaOngkir($id)
+   {
+      try {
+         $rajaOngkir = new RajaOngkir();
+         $province = $id ? $rajaOngkir->getProvinceById($id) : $rajaOngkir->getProvinces();
+         return $province;
+      } catch (\Throwable $th) {
+         throw new Exception($th->getMessage(), 500);
       }
    }
 }
